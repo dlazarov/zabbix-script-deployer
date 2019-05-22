0322c9dc348c7e0e3bffaae8addd61bb5f4d4c76
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-05-22 15:16:15 +0300

    Fixed bug caused by $

--- controller_data/external_scripts/process_restart.sh
+++ controller_data/external_scripts/process_restart.sh
@@ -12,7 +12,7 @@ if [ $res -eq 0 ]; then
         i=0
         while [ $i -le 2 ]; do
                 ps aux | grep $1 | grep -v -e "grep" -e $0 | awk '{print $2}' > $path/temp_$filename
-                $res=`comm -12 --nocheck-order <(sort $path/$filename) <(sort $path/temp_$filename) | wc -l`
+                res=`comm -12 --nocheck-order <(sort $path/$filename) <(sort $path/temp_$filename) | wc -l`
                 if [ $res -ne 0 ]; then
                         break
                 fi
