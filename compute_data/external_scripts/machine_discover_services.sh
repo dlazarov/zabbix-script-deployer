1a27e2f69767c7131a1c9a0ee24e65fb1cce425c
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-06-18 14:10:07 +0300

    Add pattern to skip ceph-osd@all service from discovery

--- compute_data/external_scripts/machine_discover_services.sh
+++ compute_data/external_scripts/machine_discover_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-services=`systemctl list-units -t service | grep "barbican\|ceilometer\|ceph\|cinder\|filebeat\|etcd\|glance\|gnocchi\|heat\|keystone\|magnum\|memcached\|neutron\|nova\|ovsdb\|apache2\|mysql\|rabbitmq-server\|vault\|libvirtd" | grep -v "juju" | grep -o "\w.*.service"`
+services=`systemctl list-units -t service | grep "barbican\|ceilometer\|ceph\|cinder\|filebeat\|etcd\|glance\|gnocchi\|heat\|keystone\|magnum\|memcached\|neutron\|nova\|ovsdb\|apache2\|mysql\|rabbitmq-server\|vault\|libvirtd" | grep -v "juju\|ceph-osd@all" | grep -o "\w.*.service"`
 
 echo "{"
 echo "     \"data\":["
