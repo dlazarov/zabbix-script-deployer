c275a84ec3c3c2c92a97032fabdc39db77af30f1
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-06-20 15:21:05 +0300

    Use lxd_index in ceph storage stats

--- controller_data/external_scripts/ceph_storage_stats.sh
+++ controller_data/external_scripts/ceph_storage_stats.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-containerId="juju-0f940e-0-lxd-2"
-str=`/usr/bin/sudo -u ubuntu lxc exec $containerId -- sh -c "sudo ceph df -f json-pretty" |grep "total_" | awk '{print $2}' | sed 's/,//'`
-{ read total; read used; read available;} <<< "${str}"
-printf %.2f\\n "$(( 100 * used / total  ))"
+
+container_id=`grep "ceph-mon" /etc/zabbix/external_scripts/lxd_index | awk -F "," '{print $2}'`
+# Value depends on argument passed from zabbix (total_bytes, total_avail_bytes, total_used_bytes)
+value=`lxc exec "$container_id" -- sh -c "ceph df -f json-pretty" | grep "$1" | awk '{print $2}' | sed 's/,//'`
+# Returned value is converted to Gigabytes
+expr $value / 1024 / 1024 / 1024
