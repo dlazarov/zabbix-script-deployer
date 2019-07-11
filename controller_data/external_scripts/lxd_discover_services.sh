1e08c37e4526e612b370dda17da3ee0eca40f92f
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-07-11 15:13:15 +0300

    Add unit_name macro to service

--- controller_data/external_scripts/lxd_discover_services.sh
+++ controller_data/external_scripts/lxd_discover_services.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-container=`echo $1|awk -F '.' '{print $2}'`
+container=$(echo $1 | awk -F '.' '{print $2}')
+unit_name=$(grep $container /etc/zabbix/external_scripts/lxd_index | awk -F ',' '{print $1}')
 
-services=`lxc exec $container -- systemctl list-units -t service | grep "barbican\|ceilometer\|ceph\|cinder\|etcd\|glance\|gnocchi\|heat\|keystone\|magnum\|memcached\|neutron\|nova\|ovsdb\|apache2\|mysql\|rabbitmq-server\|vault" | grep -v "juju" | grep -o "\w.*.service"`
+services=$(lxc exec $container -- systemctl list-units -t service | grep "barbican\|ceilometer\|ceph\|cinder\|etcd\|glance\|gnocchi\|heat\|keystone\|magnum\|memcached\|neutron\|nova\|ovsdb\|apache2\|mysql\|rabbitmq-server\|vault" | grep -v "juju" | grep -o "\w.*.service")
 
 echo "{"
-echo "     \"data\":["
+echo "\"data\":["
 
 comma=""
 for service in $services; do
-    echo "     $comma{"
-    echo "           \"{#SERVICE}\":\"$service\""
-    echo "     }"
+    echo "$comma{"
+    echo "\"{#SERVICE}\":\"$service\","
+    echo "\"{#UNIT_NAME}\":\"$unit_name\","
+    echo "\"{#CONTAINER}\":\"$container\""
+    echo "}"
     comma=","
 done
 
-echo "     ]"
+echo "]"
 echo "}"
