44c70d3b2b5a4c134932fcceda8418d5597df8f5
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-08-22 12:17:35 +0300

    Removed comma from output

--- controller_data/external_scripts/lxd_service_status.sh
+++ controller_data/external_scripts/lxd_service_status.sh
@@ -6,4 +6,4 @@ service=$2
 active_state=$(lxc exec $container_id -- systemctl show -p ActiveState $service | awk -F "=" '{print $2}')
 sub_state=$(lxc exec $container_id -- systemctl show -p SubState $service | awk -F "=" '{print $2}')
 
-printf "%s,%s" "$active_state" "$sub_state"
+printf "%s %s" "$active_state" "$sub_state"
