f23ba71331f517e0a45d4577ce25eee964149cb1
dlazarov <6955442+dlazarov@users.noreply.github.com>
2019-08-26 14:14:45 +0300

    Container DNS check refactorization

--- controller_data/external_scripts/lxd_dns_server_check.sh
+++ controller_data/external_scripts/lxd_dns_server_check.sh
@@ -1,44 +1,56 @@
 #!/bin/bash
 
 container=$1
-dns_ip=$2
 
-
-/usr/bin/lxc exec $container -- systemd-resolve --status | awk -v dns_ip="$dns_ip" '
-
-BEGIN {
-        FS=":"
-        go = 0
-        ind = 0
-}
-
+interfaces=$(lxc exec $container -- cat /etc/netplan/99-juju.yaml | awk '
 {
-        if ($0 ~ /DNS Servers:/) {
-                go = 1
-                gsub(/ /,"",$2)
-                ips[ind]=$2
-                ind += 1
-                next
-        }
-        if (go == 1) {
-                if ($0 !~ /Link|^$/) {
-                        gsub(/ /,"")
-                        ips[ind]=$0
-                        ind += 1
-                }
-                else {
-                        exit
-                }
-        }
+        if ($0 ~ /eth[0-9]/)
+                interface_name=substr($1,1,4)
 
+        if ($0 ~ /- 10.224.24|- 10.224.25|- 10.224.49/)
+                valid = 1
+        else
+                valid = 0
+
+        if (valid)
+                interfaces=interfaces interface_name " "
 }
 
 END {
-        for (i in ips) {
-                if (dns_ip == ips[i]) {
-                        print 1
-                        exit
+        print interfaces
+}')
+
+output=""
+
+for eth in $interfaces; do
+        dns_addr=$(lxc exec $container -- systemd-resolve --status | grep -F "${eth})" -A8 | awk '
+        BEGIN {
+                dns_ip[0] = "10.224.10.4"
+                dns_ip[1] = "10.224.10.56"
+                dns_ip[2] = "10.224.10.186"
+        }
+        {
+                for (ip in dns_ip) {
+                        if ($0 ~ dns_ip[ip]) {
+                                dns_ip[ip] = "ok"
+                        }
                 }
         }
-        print 0
-}'
+        END {
+                for (ip in dns_ip) {
+                        if (dns_ip[ip] != "ok")
+                                missing_ip=missing_ip " " dns_ip[ip]
+                }
+                if (missing_ip)
+                        print missing_ip
+
+        }')
+
+        if [[ $dns_addr ]]; then
+                output="${output} ${eth}:${dns_addr}"
+        else
+                echo "ok"
+        fi
+
+done
+echo $output
