741bf02db45e338af8316abda35ad78b82086e58
dlazarov <6955442+dlazarov@users.noreply.github.com>
2020-01-21 14:24:29 +0200

    Missing flag for sh

--- controller_data/external_scripts/ceph_pool_read_iops.sh
+++ controller_data/external_scripts/ceph_pool_read_iops.sh
@@ -2,6 +2,6 @@
 pool_name=$1
 
 container_id=`grep "ceph-mon" /etc/zabbix/external_scripts/lxd_index | awk -F "," '{print $2}'`
-read_iops=$(lxc exec $container_id -- sh "ceph osd pool stats $pool_name -f json | | jq -r '.[].client_io_rate.read_op_per_sec'")
+read_iops=$(lxc exec $container_id -- sh -c "ceph osd pool stats $pool_name -f json | jq -r '.[].client_io_rate.read_op_per_sec'")
 
 echo $read_iops
